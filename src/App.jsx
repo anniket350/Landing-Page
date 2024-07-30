ef0655365cb016520a9a145549d5b56a8f62e185
anniket350 <aniketrai350@gmail.com>
2024-07-30 16:42:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/HeroSection";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation></Navigation>
+      <HeroSection></HeroSection>
+    </div>
+  );
+};
+
+export default App;
